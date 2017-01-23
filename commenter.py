4951cd6bf83dfe2b753f10f8174af7954c3ad36d
Bob Holt <bob@bocoup.com>
2017-01-23 10:27:34 -0500

    retrieve public key from Travis

--- commenter.py
+++ commenter.py
@@ -1,9 +1,31 @@
+import ConfigParser
+import requests
+
 from flask import Flask
 app = Flask(__name__)
+
+
+config = ConfigParser.ConfigParser()
+config.readfp(open(r'config.txt'))
+TRAVIS_URL = config.get('Travis', 'TRAVIS_URL')
+
+
+def _get_travis_public_key():
+    """ Return the PEM-encoded public key from Travis CI /config endpoint."""
+    response = requests.get("%s/config" % TRAVIS_URL, timeout=10.0)
+    response.raise_for_status()
+    return response.json()['config']['notifications']['webhook']['public_key']
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+
+@app.route('/stability/travis', methods=['POST'])
+def travis():
+    """Respond to Travis webhook."""
+    return _get_travis_public_key()
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
